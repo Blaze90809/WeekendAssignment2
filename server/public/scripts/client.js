7468cbcf3138f94c79579d115e990de939e98cb8
Blaze Fugina <blaze.fugina@gmail.com>
2017-10-15 18:19:04 -0500

    basic functionality completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -27,6 +27,30 @@ $.ajax({
         number2: y,
         type: type
     }
+})
+.done(function(response){
+    console.log('successfully sent math question');
+    getMathAnswer();
+})
+}
+
+function getMathAnswer() {
+    $.ajax({
+        method: 'GET',
+        url: '/respondcalc'
+    })
+    .done(function(response) {
+        console.log('successful response:' + response);
+        appendMathAnswer(response);
+    })
+    .fail(function(msg, status){
+console.log('error response: ', msg, status)
+   
 });
 }
-app.get(/)
+function appendMathAnswer(answer){
+    answer = parseInt(answer.answer);
+    $('#answer').empty();
+    $('#answer').html('<p>' + 'answer: ' + answer + '</p>');
+    console.log('<p>' + 'answer: ' + answer + '</p>');
+};
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = 5000;
 var bodyParser = require('body-parser');
-var mathWork = require()
+var mathWork = require('./module/math.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -12,13 +12,52 @@ app.listen(port, function(){
     console.log('listening on port', port);
 });
 
-
-app.post("/numbersin", function(req, res){
-var x = parseInt(req.body.number1);
-var y = parseInt(req.body.number2);
-var type = req.body.type;
+var x;
+var y;
+var type;
+app.post('/numbersin', function(req, res){
+x = parseInt(req.body.number1);
+y = parseInt(req.body.number2);
+type = req.body.type;
+res.sendStatus(201);
 console.log('number 1 is: ' + x);
 console.log('number 2 is: ' + y);
 console.log('the type is: ' + type);
-res.sendStatus(200);
+return x;
+return y;
+return type;
 });
+
+var answer;
+function mathCalculation(){
+    console.log('math function');
+   if (type === 'add'){
+   answer = x + y;
+   console.log('add answer:' + answer);
+   return answer;
+   }
+   else if (type === 'subtract'){
+   answer = x - y;
+   console.log('subtract answer:' + answer);
+   return answer;
+   }
+   else if (type === 'divide'){
+       answer = x / y;
+       console.log('divide answer:' + answer);
+       return answer;
+   }
+   else if (type === 'multiply'){
+       answer = x * y;
+       console.log('multiply answer:' + answer);
+       return answer;
+   }
+   
+    
+}
+
+
+app.get('/respondcalc', function (req, res){
+    mathCalculation();
+    res.send({answer});
+    console.log(answer);
+    });
